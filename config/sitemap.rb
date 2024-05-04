9bbb36f5d08d900a77cb8af316589a00121fabfa
yuya <yuya.kyutech@gmail.com>
2024-05-04 15:51:38 +0900

    add sitemap generation logic

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -3,4 +3,9 @@ SitemapGenerator::Sitemap.default_host = "https://www.example.com"
 SitemapGenerator::Sitemap.compress = false
 
 SitemapGenerator::Sitemap.create do
+  add '/articles'
+
+  Article.find_each do |article|
+    add "/articles/#{article.id}", :lastmod => article.updated_at
+  end
 end
--- spec/config/sitemap_spec.rb
+++ spec/config/sitemap_spec.rb
@@ -1,4 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'SitemapGenerator' do
+  subject(:generate_sitemap) { SitemapGenerator::Interpreter.run }
+
+  let(:file_path) { Rails.root.join('public', 'sitemap.xml') }
+  let(:host) { 'https://www.example.com' }
+
+  after { File.delete(file_path) }
+
+  it 'generates sitemap.xml' do
+    generate_sitemap
+
+    expect(File.exist?(file_path)).to be true
+  end
+
+  it 'includes article URLs' do
+    articles = create_list(:article, 3)
+
+    generate_sitemap
+
+    file_contents = File.read(file_path)
+    expect(file_contents).to include("<loc>#{host}/articles</loc>")
+    articles.each do |article|
+      expect(file_contents).to include("<loc>#{host}/articles/#{article.id}</loc>")
+    end
+  end
 end
