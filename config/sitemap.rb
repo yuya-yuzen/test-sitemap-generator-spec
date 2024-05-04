c8f9074e58394fa335d60d7e4b437e9522d6221e
yuya <yuya.kyutech@gmail.com>
2024-05-04 15:32:38 +0900

    install sitemap_generator

---
+++ config/sitemap.rb
@@ -0,0 +1,6 @@
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "https://www.example.com"
+SitemapGenerator::Sitemap.compress = false
+
+SitemapGenerator::Sitemap.create do
+end
